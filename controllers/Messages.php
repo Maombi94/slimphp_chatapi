0a2440031a0a434b056eeb24a2e2afce0615fa44
amosmaombi <maombibetets@gmail.com>
2022-01-30 22:48:53 +0300

    Add function to send files with chat API

--- controllers/Messages.php
+++ controllers/Messages.php
@@ -31,6 +31,38 @@ $app->get('/SendMessage', function (Request $request, Response $response){
 
 
 
+$app->get('/SendFileMessage', function (Request $request, Response $response){
+
+             $apiurl = 'YOUR_API_KEY_FROM_WHATSAPP_CHAT_API';
+             $token = 'YOUR_TOKEN';
+
+            $chatId = '255655007457@c.us';
+            $caption =  'Hello there this is the caption';
+            $method = 'sendFile'; //
+            $path = 'https://cutt.ly/1Oji2Vw';
+            $filename = 'Image Demo';
+  
+            $data = array('chatId' => $chatId,'body' => $path,'filename' => $filename,'caption' => $caption);
+
+
+            $url = $apiurl . $method . '?token=' . $token;
+           
+            if (is_array($data)) {
+                $data = json_encode($data);
+            }
+            $options = stream_context_create(['http' => [
+                    'method' => 'POST',
+                    'header' => 'Content-type: application/json',
+                    'content' => $data]]);
+            $response_message = file_get_contents($url, false, $options);
+            echo $response_message;
+});
+
+
+
+
+
+
 
 
 
